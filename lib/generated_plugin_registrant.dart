9e71f7a67cf891da0b14384e80681d8cf1f45e95
jvietz <justin.vietz@eventim.de>
2020-07-28 10:21:24 +0200

    Added effective dart. Bumped Version of Hive.

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -5,11 +5,14 @@
 // ignore: unused_import
 import 'dart:ui';
 
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:soundpool_web/soundpool_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
+  SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
   SoundpoolPlugin.registerWith(registry.registrarFor(SoundpoolPlugin));
   registry.registerMessageHandler();
 }
